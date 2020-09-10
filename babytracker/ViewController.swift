fc1ac1b56aa810c98d710cb140fb77a0fdcc696b
Ai-Lyn Tang <ailyntang@gmail.com>
2020-09-10 16:29:24 +1000

    Initial project setup

---
+++ babytracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  babytracker
+//
+//  Created by Empower on 10/9/20.
+//  Copyright © 2020 WhimLyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
