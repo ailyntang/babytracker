c71c78a85c451b1df33e6a24cdd84ae829747e57
Ai-Lyn Tang <ailyntang@gmail.com>
2020-09-11 17:11:52 +1000

    Add custom table view cell for home screen

---
+++ Source/Home/HomeTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTableViewCell.swift
+//  babytracker
+//
+//  Created by Ai-Lyn Tang on 11/9/20.
+//  Copyright © 2020 WhimLyn. All rights reserved.
+//
+
+import UIKit
+
+final class HomeTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var detailLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    
+}
