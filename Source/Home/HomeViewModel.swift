bdafd2013f2d45cd234666e73edc8df283d0a888
Ai-Lyn Tang <ailyntang@gmail.com>
2020-09-18 10:55:54 +1000

    Add feeding and diaper cell view models

--- Source/Home/HomeViewModel.swift
+++ Source/Home/HomeViewModel.swift
@@ -10,9 +10,19 @@ import Foundation
 
 struct HomeViewModel {
     
-    private(set) lazy var cellViewModels = [sleepingCellViewModel]
+    private(set) lazy var cellViewModels = [sleepingCellViewModel,
+                                            feedingCellViewModel,
+                                            diaperCellViewModel]
     
     private let sleepingCellViewModel = HomeCellViewModel(titleLabel: "Sleeping",
                                                           detailLabel: "2h 38m - 3h 02m ago",
                                                           durationLabel: "00:46:23")
+    
+    private let feedingCellViewModel = HomeCellViewModel(titleLabel: "Feeding",
+                                                         detailLabel: "(L) 0m, (R*) 3m - 1h 02m ago",
+                                                         durationLabel: "00:06:23")
+    
+    private let diaperCellViewModel = HomeCellViewModel(titleLabel: "Diaper",
+                                                        detailLabel: "Both - 0h 02m ago",
+                                                        durationLabel: "")
 }
