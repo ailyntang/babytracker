c180c7c09da36840c3f8f3d8ed4457e5163ebe73
Ai-Lyn Tang <ailyntang@gmail.com>
2020-10-28 14:29:31 +1100

    Add custom color, cyan, to `UIColor`

--- Source/Extensions/UIColor+Extensions.swift
+++ Source/Extensions/UIColor+Extensions.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 WhimLyn. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let cyan = UIColor.fromRGBValues(red: 111, green: 207, blue: 229, alpha: 1)
+}
+
+extension UIColor {
+    
+    static func fromRGBValues(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        let newRed = red / 255
+        let newGreen = green / 255
+        let newBlue = blue / 255
+        return UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: alpha)
+    }
+}
